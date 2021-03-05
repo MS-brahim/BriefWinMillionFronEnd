92ba960f8bb58939d4ff7cf5e2e2ac21ed578e0a
BrahiMouissi <57224320+MS-brahim@users.noreply.github.com>
2021-03-05 17:03:27 +0100

    first commit

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+export * from './Home';
+export * from './participant/Login';
+export * from './participant/SignUp';
+export * from './participant/Account';
+export * from './admin/LogAdmin';
+export * from './admin/Dashboard';
+export * from './admin/Questions.jsx';
